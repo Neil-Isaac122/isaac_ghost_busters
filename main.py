162f99ab050b81e6cea2dda4bb2c5f4deeecb22c
Neil-Isaac122 <neil.isaac28@bcp.org>
2025-10-31 13:12:20 -0700

    added files for first time

---
+++ main.py
@@ -0,0 +1,124 @@
+#Neil Isaac
+#import necessary modules
+#core game loop
+#input
+#update
+#draw
+#sources:
+# https://chatgpt.com/g/g-p-68ffb42bcbc88191b18d019f16717c3d-computer-programming-project-neil/c/68ffb465-8ee4-832d-ba0a-39d2d01acab6 : for chasing
+
+
+#Imports all the needed modules to do stuff and imports code from our other files
+import math
+import random
+import sys
+import pygame as pg
+from settings import *
+from sprites import *
+from os import path 
+from utils import *
+
+#creates class Game and functions
+health = 100
+class Game:
+   def __init__(self):
+      pg.init()
+      #sets up a clock for fps and time and set
+      self.clock = pg.time.Clock()
+      self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+      pg.display.set_caption("Neil Isaac's awesome game!!!!!")
+      self.playing = True
+   
+   #  sets up a game folder directory path that uses the currend foler and contains THIS file
+   # gives the Game class a map property which uses the Map class to go through the level1.txt file
+   def load_data(self):
+      self.game_folder = path.dirname(__file__)
+      self.map = Map(path.join(self.game_folder, 'level1.txt'))
+
+   def draw_text(self, surface, text, size, color, x, y):
+      font_name = pg.font.match_font('arial')
+      font = pg.font.Font(font_name, size)
+      text_surface = font.render(text, True, color)
+      text_rect = text_surface.get_rect()
+      text_rect.midtop = (x,y)
+      surface.blit(text_surface, text_rect)
+
+   def new(self):
+      # the sprite Group allows us to upate and draw sprite in grouped batches
+      self.load_data()
+      # create all sprite groups
+      self.all_sprites = pg.sprite.Group()
+      self.all_mobs = pg.sprite.Group()
+      self.all_coins = pg.sprite.Group()
+      self.all_walls = pg.sprite.Group()
+      #instantiation of a class
+
+      
+      for row, tiles, in enumerate(self.map.data):
+         print(row)
+         for col, tile, in enumerate(tiles):
+            print(col)
+            if tile == '1':
+               Wall(self, col, row, "")
+
+            if tile == 'C':
+               Coin(self, col, row)
+            elif tile == 'P':
+               self.player = Player(self, col, row)
+            elif tile == 'M':
+               Mob(self, col, row)
+
+
+
+      self.all_sprites.add(self.player)
+      
+
+   def run(self):
+      while self.playing == True:
+         self.dt = self.clock.tick(FPS) / 1000
+         # input
+         self.events()
+         # process
+         self.update()
+         # output
+         self.draw()
+      pg.quit()
+
+   def events(self):
+      for event in pg.event.get():
+        if event.type == pg.QUIT:
+          print("this is happening")
+          self.playing = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+           #makes sure u can get input from clicking
+           print("I can get input from mousey mouse mouse mousekerson")
+   def update(self):
+      self.all_sprites.update()
+      #Time in seconds
+      seconds = pg.time.get_ticks()//1000
+      #countdown by subtractiing from total seconds
+      countdown = 20
+      self.time = countdown - seconds
+      if self.time <= 0:
+         pg.quit
+
+
+   def draw(self):
+      self.screen.fill(WHITE)
+      self.all_sprites.draw(self.screen)
+      self.draw_text(self.screen, "Health:"+str(self.player.health), 20, BLACK, 30, 20) 
+      self.draw_text(self.screen, "Score:"+str(self.player.score), 20, BLACK, 30, 40) 
+      self.draw_text(self.screen, "Time:"+ str(self.time), 20, BLACK, 30, 60)
+      pg.display.flip()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+#    creating an instance or instantiating the Game class
+   g = Game()
+   g.new()
+   g.run()
---
+++ sprites.py
@@ -0,0 +1,253 @@
+import pygame as pg
+from pygame.sprite import Sprite
+from settings import *
+from random import randint
+from utils import Cooldown
+from random import choice
+vec = pg.math.Vector2
+
+# The sprites module contains all the sprites
+# Sprites incldue: player, mob - moving object
+
+
+#creates new class bullet which shoots and breaks walls
+class Bullet(Sprite):
+    def __init__(self, game, x, y, direction):
+        self.game = game
+        self.groups = game.all_sprites
+        Sprite.__init__(self, self.groups)  
+        self.image = pg.Surface((TILESIZE[0]/2, TILESIZE[1]/2))
+        self.image.fill((255, 192, 203))
+        self.rect = self.image.get_rect()
+        self.vel = vec(0,0)
+        self.dir = direction
+        self.pos = vec(x+(TILESIZE[0]/4), y+(TILESIZE[0]/4))
+        self.speed = 500
+    def collide(self):
+        hits = pg.sprite.spritecollide(self, self.game.all_walls, True)
+        if hits:
+            self.kill()
+
+    def update(self):
+        if self.dir == "up":
+            self.vel.y = -self.speed*self.game.dt
+        elif self.dir == "down":
+            self.vel.y = self.speed*self.game.dt
+        elif self.dir == "right":
+            self.vel.x = self.speed*self.game.dt
+        elif self.dir == "left":
+            self.vel.x = -self.speed*self.game.dt
+        self.pos += self.vel
+        self.rect.x = self.pos.x
+        self.rect.y = self.pos.y
+
+
+#Creates player by creating a class
+class Player(Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((32, 32))
+        self.image.fill((GREEN))
+        self.rect = self.image.get_rect()
+        # self.rect.x = x * TILESIZE[0]
+        # self.rect.y = y * TILESIZE[1]
+        self.vel = vec(0,0)
+        self.pos = vec(x,y) * TILESIZE[0]
+        self.speed = 300
+        self.health = 100
+        self.score = 0
+        self.cd = Cooldown(1000)
+        self.bcd = Cooldown(250)
+        self.lastdir = "up"
+        
+    def update(self):
+        pass
+    def get_keys(self):
+        self.vel = vec(0,0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_SPACE]:
+            if self.bcd.ready():
+                self.bcd.start()
+                Bullet(self.game, self.rect.x, self.rect.y, self.lastdir)
+        if keys[pg.K_w]:
+            self.vel.y = -self.speed*self.game.dt
+            self.lastdir = "up"
+            # self.rect.y -= self.speed
+        if keys[pg.K_a]:
+            self.vel.x = -self.speed*self.game.dt
+            self.lastdir = "left"
+            # self.rect.x -= self.speed
+        if keys[pg.K_s]:
+            self.vel.y = self.speed*self.game.dt
+            self.lastdir = "down"
+            # self.rect.y += self.speed
+        if keys[pg.K_d]:
+            self.vel.x = self.speed*self.game.dt
+            self.lastdir = "right"
+            # self.rect.x += self.speed
+        # accounting for diagonal movement
+        if self.vel.x != 0 and self.vel.y != 0:
+            self.vel *= 0.7071
+
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
+            if hits:
+            # uses collide with walls to check if wall is moveable and then changing its velocity based on that
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                        
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                    
+                self.rect.x = self.pos.x
+            
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
+                
+
+
+    def collide_with_stuff(self, group, kill):
+        #constantly checking whether a spirte has collided with a certain group
+        hits = pg.sprite.spritecollide(self, group, kill)
+        if hits: 
+            if str(hits[0].__class__.__name__) == "Mob":
+                print("i collided with a mob")
+                if self.cd.ready():
+                    self.health -= 10
+                    self.cd.start()
+
+                if self.health == 0:
+                    pg.quit()
+            if str(hits[0].__class__.__name__) == "Coin":
+                self.score += 1    
+    def update(self):
+        self.get_keys()
+        self.pos += self.vel
+        self.rect.x = self.pos.x
+        self.collide_with_walls('x')
+        self.rect.y = self.pos.y
+        self.collide_with_walls('y')
+
+        self.collide_with_stuff(self.game.all_mobs, False)
+        self.collide_with_stuff(self.game.all_coins, True)
+        if not self.cd.ready():
+            self.image.fill(BLUE)
+            print("not ready")
+        else:
+            self.image.fill(GREEN)
+            print("ready")
+
+#Creates Mob using same code as player but not controllable with keys
+class Mob(Sprite):
+    def __init__(self, game, x, y):
+        Sprite.__init__(self)
+        self.game = game
+        self.groups = game.all_sprites, game.all_mobs
+        Sprite.__init__(self, self.groups)  
+        self.vel = vec(choice([-10, 10]),choice([-10, 10]))
+        self.pos = vec(x*TILESIZE[0], y*TILESIZE[1])
+        self.image = pg.Surface((32, 32))
+        self.image.fill((RED))
+        self.rect = self.image.get_rect()
+        self.rect.x = x * TILESIZE[0]
+        self.rect.y = y * TILESIZE[1]
+        self.speed = 10
+    def collide_with_walls(self, dir):
+            if dir == 'x':
+                hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
+                if hits:
+                    if self.vel.x > 0:
+                        self.pos.x = hits[0].rect.left - self.rect.width
+                    if self.vel.x < 0:
+                        self.pos.x = hits[0].rect.right
+                    self.rect.x = self.pos.x
+                    self.vel.x = 0
+                    # makes the mobs bounce randomly off wall using vectors
+                    # self.rect.x = self.pos.x
+                    # self.vel.x *= choice([-1, 1])
+                    
+            if dir == 'y':
+                hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
+                if hits:
+                    if self.vel.y > 0:
+                        self.pos.y = hits[0].rect.top - self.rect.height
+                    if self.vel.y < 0:
+                        self.pos.y = hits[0].rect.bottom
+                    self.rect.y = self.pos.y
+                    self.vel.y = 0
+                    # makes the mobs bounce randomly off wall using vectors
+                    # self.rect.y = self.pos.y
+                    # self.vel.y *= choice([-1, 1])
+    def chase_player(self, dir):
+        if dir == 'x':
+            if self.game.player.pos.x > self.pos.x:
+                self.vel.x = abs(0.7 * self.game.player.vel.x)
+            elif self.game.player.pos.x < self.pos.x:
+                self.vel.x = -abs(0.7 * self.game.player.vel.x)
+        if dir == 'y':
+            if self.game.player.pos.y > self.pos.y:
+                self.vel.y = abs(0.7 * self.game.player.vel.y)
+            elif self.game.player.pos.y < self.pos.y:
+                self.vel.y = -abs(0.7 * self.game.player.vel.y)
+
+
+        
+
+    def update(self):
+        #mob behavior
+        self.pos += self.vel
+        self.rect.x = self.pos.x
+        self.collide_with_walls('x')
+        self.chase_player('x')
+        self.rect.y = self.pos.y
+        self.collide_with_walls('y')
+        self.chase_player('y')
+        # if self.game.player.vel.x > self.vel.x:
+        #     self.vel.x = self.game.player.vel.x
+#Creates coin using same code as mob but it doesnt move
+class Coin(Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self.groups = game.all_sprites, game.all_coins
+        Sprite.__init__(self, self.groups)  
+        self.image = pg.Surface(TILESIZE)
+        self.image.fill((YELLOW))
+        self.rect = self.image.get_rect()
+        self.rect.x = x* TILESIZE[0]
+        self.rect.y = y* TILESIZE[1]
+        
+class Wall(Sprite):
+    def __init__(self, game, x, y, state):
+        self.groups = game.all_sprites, game.all_walls
+        Sprite.__init__(self, self.groups)
+        self.game = game
+
+        self.image = pg.Surface(TILESIZE)
+        self.image.fill(GREY)
+        self.rect = self.image.get_rect()
+        self.vel = vec(0,0)
+        self.pos = vec(x,y) * TILESIZE[0]
+        self.state = state
+    def update(self):
+        # wall
+        self.pos += self.vel
+        self.rect.x = self.pos.x
+        self.rect.y = self.pos.y
+       
+
+
+
+
+
+
